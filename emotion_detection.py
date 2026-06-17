978756685e2dad493e84a617445241194d40a672
turoprektnum <atrwanglort@proton.me>
2026-06-17 14:13:47 -0400

    added json formatting output with emotion classification with an item that shows dominant emotion.

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,16 @@
-import requests, json #imports request and json libraries
+import requests, json
 
-def emotion_detector(text_to_analyze):  #defines the function and its args
+def emotion_detector(text_to_analyze): 
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict' 
-    myobj = { "raw_document": { "text": text_to_analyze } }  #Create a dictionary with the text to be analyzed 
+    myobj = { "raw_document": { "text": text_to_analyze } } 
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(URL, json = myobj, headers=header)
-    return {response.text} #Returning results
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+    dominant_emotion = max(emotions, key=emotions.get)
+    return {'anger': anger_score , 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score, 'dominant_emotion': dominant_emotion}
